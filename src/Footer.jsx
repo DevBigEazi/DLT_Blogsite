0ea7c60bc60bc808e71fde2fb07da9937ebc074a
DevBigEazi <adesholatajudeen1@gmail.com>
2023-11-07 16:51:37 +0100

    set up packagejson file for gh-pages

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import { useStoreState } from "easy-peasy";
 
 const Footer = () => {
-    const date = new Date()
-    return (
-        <footer className='Footer'>
-            <p>
-                Copyright &copy; {date.getFullYear()}
-            </p>
-        </footer>
-    )
-}
+  const date = new Date();
+  const postCount = useStoreState((state) => state.postCount);
+  return (
+    <footer className="Footer">
+      <small>{postCount}</small>
+      <p>Copyright &copy; {date.getFullYear()}</p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
